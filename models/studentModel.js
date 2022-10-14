4f56e9da676ad5c230a1ecab5bae896b426dc3a7
Ugege Daniel <36529717+UgegeDaniel@users.noreply.github.com>
2022-10-15 00:41:57 +0100

    Add files via upload

---
+++ models/studentModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require("bcrypt")
+const Schema = mongoose.Schema
+const studentSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    userName:{
+        type: String,
+        required: true
+    },
+history: []
+}
+)
+
+//STATIC SIGN UP METHOD 
+studentSchema.statics.signup = async function (email, password, userName) {
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+    const student = await this.create({ email, password: hash, userName })
+    return student
+}
+module.exports = mongoose.model('Student', studentSchema)
