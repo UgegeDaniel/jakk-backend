8cabf0608e3684091e77f6ff6336b97de5573bcc
Daniel Ugege <ugege62@gmail.com>
2022-10-15 12:27:06 +0100

    heroku

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ const app = express();
 app.use(cors()) 
 
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
+app.use((req, res, next) => {
     console.log(req.path, req.method)
     next();
 }) 
